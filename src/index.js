f4289c61b45dfff13cadcf0568caec1b2616f5ae
KBulhak <s_katewhynot@yahoo.com>
2019-03-22 15:25:55 +0300

    feat: add function which checks if your string is balanced

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+    let stack = [];
+    str.split('').forEach(element => {
+      if (stack.length > 0 && searchForKeyValue(stack[stack.length -1], bracketsConfig) ==  element) {
+        stack.pop();
+        return;
+      }
+      stack.push(element);
+    });
+    return stack.length == 0;
+
+  function searchForKeyValue(key, bracketsConfig) {
+     let result = bracketsConfig.find(element => element[0]==key);
+    return result ? result[1]:null;
+  }
 }
+
+
+    
